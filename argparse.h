5de606deece1f85c121b5ff1bd016446fab3a13c
almaember <almaember@almaember.com>
2021-10-02 15:08:56 +0200

    Add argparse_init_buf()

--- argparse.h
+++ argparse.h
@@ -14,9 +14,38 @@ typedef struct {
 	size_t no_of_cfgs,
 	       idx,
 	       argc;
+	char **argv;
 } ArgparseBuf;
 
+/*
+ * Initialize an argparse buffer
+ * WARNING: All parameters must be non-zero.
+ * The function doesn't check for the pointers' validity at runtime!
+ * 
+ * WARNING: The function does not copy the value of cfgs. Make sure
+ * that the buffer is only used while the pointer is valid!
+ */
+void argparse_init_buf(ArgparseBuf *buf,
+                       ArgparseArgCfg cfgs[],
+                       size_t no_of_cfgs,
+                       size_t argc,
+                       char *argv[]);
+
 #ifdef ARGPARSE_IMPL
+
+void argparse_init_buf(ArgparseBuf *buf,
+                       ArgparseArgCfg cfgs[],
+                       size_t no_of_cfgs,
+                       size_t argc,
+                       char *argv[]) {
+	buf->cfgs = cfgs;
+	buf->no_of_cfgs = no_of_cfgs;
+	buf->argc = argc;
+	buf->argv = argv;
+
+	buf->idx = 0;
+}
+
 #endif
 
 #endif
