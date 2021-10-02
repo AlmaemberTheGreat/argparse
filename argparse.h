c53e7962684d27dc508ea24d266d8218329442e1
almaember <almaember@almaember.com>
2021-10-02 14:02:41 +0200

    Add basic types to argparse.h

--- argparse.h
+++ argparse.h
@@ -1,6 +1,20 @@
 #ifndef ARGPARSE_H
 #define ARGPARSE_H
 
+#include <stddef.h>
+
+typedef struct {
+	char short_name,
+	    *long_name;
+	int code;
+} ArgparseArgCfg;
+
+typedef struct {
+	ArgparseArgCfg *cfg;
+	size_t idx,
+	       argc;
+} ArgparseBuf;
+
 #ifdef ARGPARSE_IMPL
 #endif
 
